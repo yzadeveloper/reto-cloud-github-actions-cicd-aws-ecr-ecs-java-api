af1e921fc1b5d10f38fb54d8196b434072ae545e
GuillermoD <deleuzeguillermo@gmail.com>
2026-03-13 14:11:56 +0100

    testing ci/cd  push-2

--- src/main/java/org/factoriaf5/lab/controllers/HomeController.java
+++ src/main/java/org/factoriaf5/lab/controllers/HomeController.java
@@ -10,7 +10,7 @@ public class HomeController {
 
     @GetMapping("")
     public String index() {
-        return new String("Hello AWS! I like ECR & ECS. Workflow is great!");
+        return new String("Hello from github");
     }
 
 }
